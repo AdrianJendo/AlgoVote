b484472e652bdfd47228d4e3494c6aa0d1c2790e
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-18 19:31:23 -0800

    Start working on Payment page

--- frontend/src/components/Payment.jsx
+++ frontend/src/components/Payment.jsx
@@ -1,14 +1,31 @@
-import React from "react";
-import { Button, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Button, Typography, TextField } from "@mui/material";
+// import { VoteInfoContext } from "context/VoteInfoContext";
+// import {  } from "utils/AlgoFunctions";
+// import HelpIcon from "@mui/icons-material/Help";
+// import HelperTooltip from "components/HelperTooltip";
 
 const Payment = () => {
+	const [secretKey, setSecretKey] = useState("");
+
+	const isMnemonicValid = (str) => {
+		console.log(str, str.split(" "), str.split(" ").length);
+		return str.split(" ").length !== 25; // check that number of words in mnemonic is 25
+	};
+
+	const submitSecretKey = async () => {
+		// validate that secret key exists
+
+		return true;
+	};
+
 	return (
 		<div
 			style={{
 				position: "relative",
 				height: "100%",
-				width: "60%",
-				left: "20%",
+				width: "80%",
+				left: "10%",
 			}}
 		>
 			<Typography
@@ -16,9 +33,34 @@ const Payment = () => {
 				component="div"
 				sx={{ flexGrow: 1, padding: "10px" }}
 			>
-				Give Payment
+				Paste the secret key to the creator's wallet address to create
+				the vote
 			</Typography>
-			<Button>Pay here</Button>
+			<div
+				style={{
+					position: "relative",
+					alighItems: "center",
+					padding: "20px",
+				}}
+			>
+				<TextField
+					label="Secret Key"
+					placeholder="Paste Secret Key"
+					multiline
+					rows={4}
+					sx={{ width: "400px" }}
+					value={secretKey}
+					onChange={(e) => setSecretKey(e.target.value)}
+				/>
+			</div>
+			<Button
+				variant="contained"
+				onClick={submitSecretKey}
+				sx={{ mt: 1, mr: 1 }}
+				disabled={isMnemonicValid(secretKey)}
+			>
+				Submit
+			</Button>
 		</div>
 	);
 };
