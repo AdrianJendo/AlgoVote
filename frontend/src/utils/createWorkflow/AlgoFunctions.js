fc9a3dae27445ef28d3ba295817a5751121b073b
Adrian Jendo <ajendo@uwaterloo.ca>
2022-02-19 23:39:46 -0800

    Add error handling to generateAlgorandAccounts function

--- frontend/src/utils/createWorkflow/AlgoFunctions.js
+++ frontend/src/utils/createWorkflow/AlgoFunctions.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 export const generateAlgorandAccounts = async (numAccounts) => {
-	const algoAccountPromises = [];
-	for (let i = 0; i < numAccounts; i++) {
-		algoAccountPromises.push(
-			axios.post("/api/algoAccount/createAlgoAccount")
-		);
+	try {
+		const algoAccountPromises = [];
+		for (let i = 0; i < numAccounts; i++) {
+			algoAccountPromises.push(
+				axios.post("/api/algoAccount/createAlgoAccount")
+			);
+		}
+		const algoAccounts = await Promise.all(algoAccountPromises);
+		return algoAccounts.map((account) => account.data);
+	} catch (err) {
+		const error = err.response?.data?.message || err.message;
+		console.warn(error);
+		return { error };
 	}
-	const algoAccounts = await Promise.all(algoAccountPromises);
-	return algoAccounts.map((account) => account.data);
 };
