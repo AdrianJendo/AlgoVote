7c6476c63256e39001560d50c10639333b232d06
Adrian Jendo <ajendo@uwaterloo.ca>
2022-03-10 19:21:29 -0800

    Change react-router-dom navigate to href so ctrl-click works

--- frontend/src/components/base/AppBar.jsx
+++ frontend/src/components/base/AppBar.jsx
@@ -1,60 +1,43 @@
-import React, { useContext } from "react";
-import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
+import React from "react";
+import { AppBar, Box, Toolbar, Typography, Button, Link } from "@mui/material";
 import MUISwitch from "components/base/MuiSwitch";
-import { VoteInfoContext } from "context/VoteInfoContext";
-import { ParticipateContext } from "context/ParticipateContext";
-import { VoteResultsContext } from "context/VoteResultsContext";
-import changeRoute from "utils/misc/ChangeRoute";
-import { useNavigate } from "react-router-dom";
 
 const TopAppBar = ({ dark, setDark }) => {
-	const setVoteInfo = useContext(VoteInfoContext)[1];
-	const setParticipateInfo = useContext(ParticipateContext)[1];
-	const setVoteResults = useContext(VoteResultsContext)[1];
-	const navigate = useNavigate();
-
 	const toggleSwitch = () => {
 		setDark(!dark);
-	};
-
-	const nav = (route) => {
-		changeRoute(
-			navigate,
-			route,
-			setVoteInfo,
-			setParticipateInfo,
-			setVoteResults
-		);
 	};
 
 	return (
 		<Box sx={{ flexGrow: 1 }}>
 			<AppBar position="static" style={{ height: "64px" }}>
 				<Toolbar>
-					<img
-						onClick={() => nav("/")}
-						src={"/algorand.png"}
-						alt="Err"
-						height="35px"
-						style={{
-							paddingRight: "25px",
-							cursor: "pointer",
-						}}
-					/>
-					<Typography
-						variant="h6"
-						component="div"
-						sx={{ flexGrow: 1, cursor: "pointer" }}
-						onClick={() => nav("/")}
+					<Link href="/">
+						<img
+							src={"/algorand.png"}
+							alt="Err"
+							width="35px"
+							style={{
+								paddingRight: "25px",
+								cursor: "pointer",
+							}}
+						/>
+					</Link>
+					<Link
+						href="/"
+						color="inherit"
+						sx={{ flexGrow: 1 }}
+						underline="none"
 					>
-						Algo Vote
-					</Typography>
-					<Button color="inherit" onClick={() => nav("/")}>
-						Home
-					</Button>
-					<Button color="inherit" onClick={() => nav("/about")}>
-						About
-					</Button>
+						<Typography variant="h6" component="div">
+							Algo Vote
+						</Typography>
+					</Link>
+					<Link href="/" color="inherit" underline="none">
+						<Button color="inherit">Home</Button>
+					</Link>
+					<Link href="/about" color="inherit" underline="none">
+						<Button color="inherit">About</Button>
+					</Link>
 					<MUISwitch defaultChecked toggleSwitch={toggleSwitch} />
 				</Toolbar>
 			</AppBar>
