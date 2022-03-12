123ff3e27a8a6a9c1f9b03f697dfc8cbb590d0f0
Adrian Jendo <ajendo@uwaterloo.ca>
2022-03-11 22:31:06 -0800

    handle newline at end of secret key

--- frontend/src/utils/misc/IsMnemonicValid.js
+++ frontend/src/utils/misc/IsMnemonicValid.js
@@ -1,6 +1,10 @@
 const isMnemonicValid = (str) => {
 	const mnemonicArr = str.split(" ");
-	return mnemonicArr.length === 25 && mnemonicArr[24] !== ""; // check that number of words in mnemonic is 25
+	return (
+		mnemonicArr.length === 25 &&
+		mnemonicArr[24] !== "" &&
+		!mnemonicArr[24].includes("\n")
+	); // check that number of words in mnemonic is 25
 };
 
 export default isMnemonicValid;
