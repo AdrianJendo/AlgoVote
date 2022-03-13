15d183b361c41e032717f5643a8c8cbad27565f0
Adrian Jendo <ajendo@uwaterloo.ca>
2022-03-12 18:27:26 -0800

    Update IsMnemonicValid function to check for \n in entire string

--- frontend/src/utils/misc/IsMnemonicValid.js
+++ frontend/src/utils/misc/IsMnemonicValid.js
@@ -3,7 +3,7 @@ const isMnemonicValid = (str) => {
 	return (
 		mnemonicArr.length === 25 &&
 		mnemonicArr[24] !== "" &&
-		!mnemonicArr[24].includes("\n")
+		!mnemonicArr.some((word) => word.includes("\n")) // make sure there are no newline characters in the secret key
 	); // check that number of words in mnemonic is 25
 };
 
