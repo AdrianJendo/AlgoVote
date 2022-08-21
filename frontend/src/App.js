4e1804cda6a359a59a9e259383aacbcfd1b20601
Adrian Jendo <ajendo@uwaterloo.ca>
2022-08-20 22:59:53 -0400

    Update App.js styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,8 @@ import { lightTheme, darkTheme } from "theme/Themes";
 // Mui
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 // date-fns
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers";
 import { styled } from "@mui/system";
 
 // Context
@@ -27,7 +27,7 @@ import CreateCreatorAccount from "components/createCreatorAccount/CreateCreatorA
 import StickyAlert from "components/base/StickyAlert";
 
 const Dashboard = styled("div")(
-	({ theme }) => `
+  ({ theme }) => `
 		background-color: ${theme.palette.background.default};
 		width: 100%;
 		height: calc(100vh - 64px);
@@ -35,7 +35,7 @@ const Dashboard = styled("div")(
 );
 
 const StyledBackground = styled("div")(
-	({ theme }) => `
+  ({ theme }) => `
 		height: 100%;
 		background: ${theme.palette.background.paper};
 		overflow-y: hidden;
@@ -43,70 +43,54 @@ const StyledBackground = styled("div")(
 );
 
 function App() {
-	const [dark, setDark] = useState(true);
+  const [dark, setDark] = useState(true);
 
-	return (
-		<Router>
-			<ThemeProvider
-				theme={dark ? createTheme(darkTheme) : createTheme(lightTheme)}
-			>
-				<LocalizationProvider dateAdapter={AdapterDateFns}>
-					<VoteInfoProvider>
-						<ParticipateProvider>
-							<VoteResultsProvider>
-								<AlertProvider>
-									<DateValueProvider>
-										<AppBar dark={dark} setDark={setDark} />
-										<Dashboard>
-											<StyledBackground>
-												<Routes>
-													<Route
-														path="/"
-														element={
-															<ChooseOption />
-														}
-													/>
-													<Route
-														path="/createVote"
-														element={
-															<CreateWorkflow />
-														}
-													/>
-													<Route
-														path="/participateVote"
-														element={
-															<ParticipateWorkflow />
-														}
-													/>
-													<Route
-														path="/voteResults"
-														element={
-															<VoteResultsWorkflow />
-														}
-													/>
-													<Route
-														path="/about"
-														element={<About />}
-													/>
-													<Route
-														path="/createCreatorAccount"
-														element={
-															<CreateCreatorAccount />
-														}
-													/>
-												</Routes>
-											</StyledBackground>
-										</Dashboard>
-										<StickyAlert />
-									</DateValueProvider>
-								</AlertProvider>
-							</VoteResultsProvider>
-						</ParticipateProvider>
-					</VoteInfoProvider>
-				</LocalizationProvider>
-			</ThemeProvider>
-		</Router>
-	);
+  return (
+    <Router>
+      <ThemeProvider
+        theme={dark ? createTheme(darkTheme) : createTheme(lightTheme)}
+      >
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <VoteInfoProvider>
+            <ParticipateProvider>
+              <VoteResultsProvider>
+                <AlertProvider>
+                  <DateValueProvider>
+                    <AppBar dark={dark} setDark={setDark} />
+                    <Dashboard>
+                      <StyledBackground>
+                        <Routes>
+                          <Route path="/" element={<ChooseOption />} />
+                          <Route
+                            path="/createVote"
+                            element={<CreateWorkflow />}
+                          />
+                          <Route
+                            path="/participateVote"
+                            element={<ParticipateWorkflow />}
+                          />
+                          <Route
+                            path="/voteResults"
+                            element={<VoteResultsWorkflow />}
+                          />
+                          <Route path="/about" element={<About />} />
+                          <Route
+                            path="/createCreatorAccount"
+                            element={<CreateCreatorAccount />}
+                          />
+                        </Routes>
+                      </StyledBackground>
+                    </Dashboard>
+                    <StickyAlert />
+                  </DateValueProvider>
+                </AlertProvider>
+              </VoteResultsProvider>
+            </ParticipateProvider>
+          </VoteInfoProvider>
+        </LocalizationProvider>
+      </ThemeProvider>
+    </Router>
+  );
 }
 
 export default App;
